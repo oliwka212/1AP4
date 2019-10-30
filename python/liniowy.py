30cdd455b1e5d6c3c39bea3d891baa8ed207d8b8
oliwka212 <oliwia.kowalska@lo1.sandomierz.pl>
2019-10-30 11:34:03 +0100

    pierwszy skrypt Pythona

---
+++ python/liniowy.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def main(args):
+    a = 10
+    b = 5
+    print('Suma:', a + b)
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
