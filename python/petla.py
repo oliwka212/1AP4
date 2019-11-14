1ac8b377c963cbcab7051d14f15d9f8210575d58
oliwka212 <oliwia.kowalska@lo1.sandomierz.pl>
2019-11-14 12:34:13 +0100

    algorytny iteracyjne

--- python/petla.py
+++ python/petla.py
@@ -2,17 +2,27 @@
 # -*- coding: utf-8 -*-
 
 
-def sumuj_parzyste():
+def sumuj_parzyste(start, stop):
     suma = 0
-    for i in range(0, 101, 2):
+    for i in range(start, stop+1, 2):
         #liczba = int(input("Podaj liczbę: "))
         suma = suma + i
         
     print(suma)
 
 
+def drukuj_nieparzyste(start, stop):
+    if start % 2== 0:
+        start= start+1
+    for i in range(start, stop+1):
+        if i % 2 ==1:
+          print(i)
+        
 def main(args):
-    sumuj_parzyste()
+    start= int(input("Podaj początek zakresu: "))
+    stop = int(input("Podaj koniec zakresu: "))
+    #sumuj_parzyste(start, stop)
+    drukuj_nieparzyste(start, stop)
    
     return 0
 
