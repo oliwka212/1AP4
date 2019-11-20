e6be21239dc3538ba1cc83d1f785f7f07012b583
oliwka212 <oliwia.kowalska@lo1.sandomierz.pl>
2019-11-20 11:41:26 +0100

    petla while

---
+++ python/zadanie2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+
+
+def main(args):
+    start= int(input('Podaj liczbę m: '))
+    stop= int(input('Podaj liczbę n: '))
+    for i in range(start, stop+1):
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
