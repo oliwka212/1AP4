a486ea5ed8e57f733bc40aad4b63b5fe115ff612
oliwka212 <oliwia.kowalska@lo1.sandomierz.pl>
2019-12-05 12:36:14 +0100

    placa

---
+++ C++/placa.cpp
@@ -0,0 +1,26 @@
+/*
+ * placa.cpp
+ 
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+    float ln, stawka, płaca
+    int a;
+    cout <<"Podaj liczbę nadgodzin:" <<endl;
+    cin>>ln;
+    cout <<"Podaj stawkę:" <<endl;
+    cin>>stawka; 
+    if ( ln>=30)
+    cout<<(ln*)
+    
+    else (ln>30)
+    
+{
+	
+	return 0;
+}
+
---
+++ C++/wieksza.cpp
@@ -0,0 +1,26 @@
+/*
+ * wieksza.cpp
+ 
+ */
+
+
+#include <iostream>
+using namespace std;
+    
+        
+
+int main(int argc, char **argv) {
+    int a, b;
+    cout <<"Podaj liczbę a:" <<endl;
+    cin>>a;
+    cout <<"Podaj liczbę b:" <<endl;
+    cin>>b;
+	}if (a > b){
+        cout<<"a>b"<<endl;
+    }else if (a=b)
+        cout<<"a=b"<<endl;
+    }else (b>a)
+       cout<<"b>a"<<endl;
+	return 0;
+}
+
