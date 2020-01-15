bd4efbd5f4ce55ebaaabb795ad82ee2c24176878
oliwka212 <oliwia.kowalska@lo1.sandomierz.pl>
2020-01-15 11:40:21 +0100

    iksde

--- C++/srednia.cpp
+++ C++/srednia.cpp
@@ -12,40 +12,37 @@ void pobierz0ceny(int tb[], int n) {
 	cout<< "Podaj ocene:";
     cin>>ocena;
     
-    while (n > 0);
+    while (n > 0){
     
-    if(ocena > 0 && ocena < 7){
-     tb[i]= ocena;
-     i++;
-     n--;
+        if(ocena > 0 && ocena < 7){
+            tb[i]= ocena;
+            i++;
+            n--;
      
      
-    }
-  }      
+        }
+    }      
 }
 float liczsrednia(int[], int n){
     int suma = 0;
     for (int i=0; i<n; i++){
-    cout<<tb[i]<<"";
-    suma= suma + tb[i];
-    
-    
-    
-    
+        //cout<<tb[i]<<"";
+        suma= suma + tb[i];
    } 
     
     
-    
+ return (float)suma / (float)n; 
 }
 
 
 
 
-void drukuj(int tb[], int n) {
-    for (int i=0; i<n; i++){
-    cout<<tb[i]<<"";
-   } 
-}
+//void drukuj(int tb[], int n) {
+    //for (int i=0; i<n; i++){
+   // cout<< suma + tb[i];
+  // } 
+ //  return suma / n;
+//}
 
 
 
@@ -61,7 +58,10 @@ int main(int argc, char **argv){
     cin>>n;
     int oceny[n];
     pobierz0ceny(oceny, n);
-    drukuj(oceny,n);
+    float srednia = Liczsrednia(oceny, n)
+    cout << "Srednia ocen: " <<srednia << endl;
+    cout << "Srednia ocen: " <<liczsrednia(ocena,n) << endl;
+    
     
    }
 	return 0;
