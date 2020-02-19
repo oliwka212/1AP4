09e3e8c3a7000649b15f0eeaba2ddb04d3bc5c5c
oliwka212 <oliwia.kowalska@lo1.sandomierz.pl>
2020-02-19 11:40:55 +0100

    okkk

--- C++/cw2tab.cpp
+++ C++/cw2tab.cpp
@@ -5,24 +5,41 @@
 
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 void wypelnij(int t[][10], int w, int k, int n) {
     srand(time(NULL));
-    cout << rand() % (n + 1) << endl;
+    for(int i=0; i < w; i++){
+        for(int j=0; j<k; j++) {
+            t[i][j] = rand() % (n + 1);
+            cout << i << "," << j << " " << t[i][j] << endl;   
+            
+        }
+    }
 }
-
-
+void sumujW(int t[][10], int w, int k){
+    int sumaW = 0;
+    for(int i=0; i < w; i++) {
+        for(int j=0; j<k; j++) {
+            
+                cout << setw(4) << t[i][j];  
+                sumaW += t[i][j]; 
+        }
+        cout << setw(6) << endl;
+    }
+}
 int main(int argc, char **argv)
 {
 	int n = 0;
-    //cout << "Podaj wartość maksymalną: "; 
-    //cin >> n;
+    cout << "Podaj wartość maksymalną: "; 
+    cin >> n;
     
     int w = 5;
     int k = 10;
     int tab[w] [10];
     wypelnij(tab, w, k, 100);
+    sumujW(tab, w, k);
 	return 0;
 }
 
